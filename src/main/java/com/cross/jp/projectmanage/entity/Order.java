026ec564525e257c7fbafd80bfe74ba544f87b11
PCuser <cyborg.kohei002@outlook.jp>
2023-09-09 14:49:52 +0900

    OrderのEntityクラスの作成

--- src/main/java/com/cross/jp/projectmanage/entity/Order.java
+++ src/main/java/com/cross/jp/projectmanage/entity/Order.java
@@ -1,4 +1,31 @@
 package com.cross.jp.projectmanage.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * orderテーブルの情報
+ */
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //自動生成される
+    private Integer id;
+
+    @Column(name = "item_category")
+    private Integer itemCategory;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "amount")
+    private Integer amount;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
