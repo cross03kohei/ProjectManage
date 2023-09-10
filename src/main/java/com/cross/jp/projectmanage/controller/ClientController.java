5c3ca6044c550f99203f5b2aff61452f583314ad
PCuser <cyborg.kohei002@outlook.jp>
2023-09-11 01:10:18 +0900

    顧客登録機能を実装

--- src/main/java/com/cross/jp/projectmanage/controller/ClientController.java
+++ src/main/java/com/cross/jp/projectmanage/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.cross.jp.projectmanage.controller;
 
 import com.cross.jp.projectmanage.dto.ClientDto;
+import com.cross.jp.projectmanage.entity.Client;
 import com.cross.jp.projectmanage.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +11,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/client")
 public class ClientController {
     @Autowired
     ClientService service;
+    @GetMapping("/list")
+    public String clientList(Model model){
+        List<Client> clientList;
+        return "client_list";
+    }
     @GetMapping("/add")
     public String addClient(Model model){
         model.addAttribute("clientDto", new ClientDto());
