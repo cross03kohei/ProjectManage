cb5a486055c9cf8b9ce4a6898fe87a158ffcda36
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-09-21 15:29:20 +0900

    エラーの修正

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -1,4 +1,6 @@
 jQuery(function ($) {
+
+    $(".clientTr").empty();
     $("#search_button").click(function () {
     $.ajax({
                 url:"json",
@@ -8,9 +10,11 @@ jQuery(function ($) {
                 timespan:1000,
             })
             .done(function(data, textStatus, jqXHR){
-                console.log(data[0].name);
                 for(var i = 0; i < data.length; i++){
-
+                   $obj = $(".clientTr").clone();
+                   $obj.find(".clientName").text(data[i].name);
+                   $obj.find(".btn").val(data[i].id)
+                   $("#clientTbody").appendTo($obj);
                 }
             }).fail(function(jqXHR, textStatus, errorThrown ) {
                           alert("エラーが発生し、データが取得できませんでした");
