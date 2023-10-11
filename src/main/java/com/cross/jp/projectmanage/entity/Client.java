602702c8904bd47d125fbfb262dd66e049f51ca5
cross03kohei <140483428+cross03kohei@users.noreply.github.com>
2023-10-11 16:53:10 +0900

    バリデーション

--- src/main/java/com/cross/jp/projectmanage/entity/Client.java
+++ src/main/java/com/cross/jp/projectmanage/entity/Client.java
@@ -1,8 +1,7 @@
 package com.cross.jp.projectmanage.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
